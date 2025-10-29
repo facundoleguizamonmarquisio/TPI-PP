e4ebafc6d14150772917d965763cbaf5b996760a
eevee1370 <evelynjaimez04@gmail.com>
2025-10-29 19:33:12 -0300

    Que no se rompa mas

--- vistas/VentanaPrincipal.java
+++ vistas/VentanaPrincipal.java
@@ -9,7 +9,7 @@ import modelos.Vehiculo;
 public class VentanaPrincipal extends JFrame {
     private JComboBox<Cuenta> comboCuentas;
     private JTextField tfMarca, tfModelo, tfPatente, tfColor;
-    private JButton btnRegistrar;
+    private JButton btnAsociar, btnDesasociar, btnImprimir;
 
     public VentanaPrincipal(ArrayList<Cuenta> cuentas) {
         this.setTitle("Registro de vehículo");
@@ -17,5 +17,27 @@ public class VentanaPrincipal extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
 
+        // Botones
+        btnAsociar = new JButton("Asociar");
+        btnDesasociar = new JButton("Asociar");
+        btnImprimir = new JButton("Asociar");
+
+        // Acciones del botón
+        btnAsociar.addActionListener(e -> {
+            System.out.println("Asocie xd");
+        });
+
+        btnDesasociar.addActionListener(e -> {
+            System.out.println("Desasocie xd");
+        });
+
+        btnDesasociar.addActionListener(e -> {
+            System.out.println("Imprimi xd");
+        });
+
+        // Agregar componentes
+        add(btnAsociar);
+        add(btnDesasociar);
+        add(btnImprimir);
     }
 }
