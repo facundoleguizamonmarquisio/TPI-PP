38a00d06856568eaf189b55e15b5eb73676f8f59
eevee1370 <evelynjaimez04@gmail.com>
2025-10-30 14:52:29 -0300

    ComboBox

--- vistas/VentanaPrincipal.java
+++ vistas/VentanaPrincipal.java
@@ -1,61 +1,101 @@
 package vistas;
 
-import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
 import modelos.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
 public class VentanaPrincipal extends JFrame {
-    private JComboBox<Cuenta> comboCuentas;
-    private JTextField tfMarca, tfModelo, tfPatente, tfColor;
-    private JButton btnAsociar, btnDesasociar, btnImprimir;
+    private JComboBox<Cuenta> cbCuentas;
+    private JTextField tfEditorComboC, tfMarca, tfModelo, tfPatente, tfColor;
+    private JButton btnEliminar, btnRegistrar, btnImprimir;
 
     public VentanaPrincipal(ArrayList<Cuenta> cuentas) {
-        this.setTitle("Registro de vehículo");
-        this.setSize(550, 400);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setLayout(new FlowLayout());
+        setTitle("Registro de vehículo");
+        setSize(550, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
 
-        // ComboBox
-        comboCuentas = new JComboBox<>();
+        inicializarComponentes(cuentas);
 
-        // Botones
-        btnAsociar = new JButton("Asociar");
-        btnDesasociar = new JButton("Desasociar");
-        btnImprimir = new JButton("Imprimir");
+        inicializarEventos();
 
-        // Acciones del botón
-        btnAsociar.addActionListener(e -> {
-            System.out.println("Asocie xd");
-        });
-
-        btnDesasociar.addActionListener(e -> {
-            System.out.println("Desasocie xd");
-        });
-
-        // btnImprimir.addActionListener(e -> {
-        // System.out.println("Imprimi xd");
-        // });
-
-        // Agregar componentes
-        add(comboCuentas);
-
-        add(btnAsociar);
-        add(btnDesasociar);
-        // add(btnImprimir);
-
+        configurarLayout();
     }
 
     // Getter
     public JButton getBtnAsociar() {
-        return btnAsociar;
+        return btnRegistrar;
     }
 
     public JButton getBtnDesasociar() {
-        return btnDesasociar;
+        return btnEliminar;
     }
 
     public JButton getBtnImprimir() {
         return btnImprimir;
     }
+
+    // Métodos
+    private void inicializarComponentes(ArrayList<Cuenta> cuentas) {
+        btnRegistrar = new JButton("Registrar");
+        btnEliminar = new JButton("Eliminar");
+
+        cbCuentas = new JComboBox<>();
+        for (Cuenta cuenta : cuentas) {
+            cbCuentas.addItem(cuenta);
+        }
+        cbCuentas.setEditable(true);
+
+        JTextField tfEditorComboC = (JTextField) cbCuentas.getEditor().getEditorComponent();
+
+        tfEditorComboC.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                String texto = tfEditorComboC.getText().toLowerCase();
+                cbCuentas.hidePopup(); // Ocultar temporalmente
+
+                cbCuentas.removeAllItems();
+
+                for (Cuenta cuenta : cuentas) {
+                    if (cuenta.toString().toLowerCase().contains(texto)) {
+                        cbCuentas.addItem(cuenta);
+                    }
+                }
+
+                cbCuentas.getEditor().setItem(texto);
+                cbCuentas.showPopup();
+            }
+        });
+
+    }
+
+    private void inicializarEventos() {
+        btnRegistrar.addActionListener(e -> registrar());
+
+        btnEliminar.addActionListener(e -> eliminar());
+    }
+
+    private void configurarLayout() {
+        add(cbCuentas);
+
+        add(btnRegistrar);
+        add(btnEliminar);
+    }
+
+    // Eventos
+    private void registrar() {
+        System.out.println("Registrado");
+    }
+
+    private void eliminar() {
+        System.out.println("Eliminado");
+    }
+
 }
