1f48b927bc3e3e36f55d232d52d2daeaad8409df
eevee1370 <evelynjaimez04@gmail.com>
2025-10-30 13:17:55 -0300

    si

--- principal/Main.java
+++ principal/Main.java
@@ -23,8 +23,8 @@ public class Main {
             System.out.println("----------------------");
         }
 
-        ControladorRegistro controlador = new ControladorRegistro();
-        VentanaPrincipal vista = new VentanaPrincipal(vista);
+        VentanaPrincipal vista = new VentanaPrincipal();
+        ControladorRegistro controlador = new ControladorRegistro(vista);
 
         vista.setVisible(true);
     }
